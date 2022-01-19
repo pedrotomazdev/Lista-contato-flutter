8be178ec8e88aeb8b3c9649db9ffa1fa91e15a44
Pedro Tomaz <pedrotomazdev@gamail.com>
2022-01-19 19:58:27 -0300

    Versão final do projeto

--- lib/screens/contact_add.dart
+++ lib/screens/contact_add.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newbytebank/database/dao/contact_dao.dart';
 import 'package:newbytebank/models/contact.dart';
 
 // ignore: use_key_in_widget_constructors
@@ -11,12 +12,13 @@ class _ContactFormState extends State<ContactForm> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _accountNumberController =
       TextEditingController();
+  final ContactDao _dao = ContactDao();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Adicionar Contato"),
+        title: const Text("New Contact"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -67,7 +69,9 @@ class _ContactFormState extends State<ContactForm> {
                     final String? name = _nameController.text;
                     final int? accountNumber =
                         int.tryParse(_accountNumberController.text);
-                    _createContact(context, name, accountNumber);
+                    final Contact newContact =
+                        Contact(0, name!, accountNumber!);
+                    _dao.save(newContact).then((id) => Navigator.pop(context));
                   },
                   child: const Text("Create"),
                 ),
@@ -80,15 +84,15 @@ class _ContactFormState extends State<ContactForm> {
   }
 }
 
-void _createContact(context, name, accountNumber) {
-  if (name != null && accountNumber != null) {
-    final newContact = Contact(0, name, accountNumber);
-    Navigator.pop(context, newContact);
-    // debugPrint('$transferenciaCriada');
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text('$newContact'),
-      ),
-    );
-  }
-}
+// void _createContact(context, name, accountNumber) {
+//   if (name != null && accountNumber != null) {
+
+//     // Navigator.pop(context, newContact);
+//     // debugPrint('$transferenciaCriada');
+//     // ScaffoldMessenger.of(context).showSnackBar(
+//     //   SnackBar(
+//     //     content: Text('$newContact'),
+//     //   ),
+//     // );
+//   }
+// }
--- lib/screens/contact_form.dart
+++ lib/screens/contact_form.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:newbytebank/database/app_database.dart';
+import 'package:newbytebank/database/dao/contact_dao.dart';
 import 'package:newbytebank/models/contact.dart';
 import 'package:newbytebank/screens/contact_add.dart';
 
+class ContactList extends StatefulWidget {
+  const ContactList({Key? key}) : super(key: key);
+
+  @override
+  _ContactListState createState() => _ContactListState();
+}
+
 // ignore: use_key_in_widget_constructors
-class ContactList extends StatelessWidget {
+class _ContactListState extends State<StatefulWidget> {
+  final ContactDao _dao = ContactDao();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,51 +20,57 @@ class ContactList extends StatelessWidget {
         title: const Text('Contacts'),
       ),
       body: FutureBuilder<List<Contact>>(
-        initialData: [],
-        future: Future.delayed(const Duration(seconds: 1))
-            .then((value) => findAll()),
+        initialData: const [],
+        future: _dao.findAll(),
         builder: (context, snapshot) {
-          final List<Contact>? contacts = snapshot.data;
-          if (contacts != null) {
-            //feito if para validar nulo
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                final Contact contact = contacts[index];
-                return _ContactItem(contact);
-              },
-              itemCount: contacts.length,
-            );
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return SizedBox(
+                width: double.maxFinite,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const <Widget>[
+                    CircularProgressIndicator(),
+                    Text('Loading Contacts',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              );
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact>? contacts = snapshot.data;
+              if (contacts != null) {
+                //feito if para validar nulo
+                return ListView.builder(
+                  itemBuilder: (context, index) {
+                    final Contact contact = contacts[index];
+                    return _ContactItem(contact);
+                  },
+                  itemCount: contacts.length,
+                );
+              }
+              break;
           }
 
-          return SizedBox(
-            width: double.maxFinite,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const <Widget>[
-                CircularProgressIndicator(),
-                Text('Loading Contacts',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold)),
-              ],
-            ),
-          );
+          return const Text('Unknow Connection to Database');
         },
       ),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {
-          Navigator.of(context)
-              .push(
-                MaterialPageRoute(
-                  builder: (context) => ContactForm(),
-                ),
-              )
-              .then(
-                (newContact) => debugPrint('$newContact'),
-              ),
+        onPressed: () async {
+          await Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ContactForm(),
+            ),
+          );
+          setState(() => {});
         },
         child: const Icon(Icons.add),
       ),
